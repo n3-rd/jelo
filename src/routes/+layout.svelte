dcb021b2529303a355848ae324743f09c73907e0
n3-rd <godwinjdayo@gmail.com>
2023-12-26 02:41:39 +0100

    Initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+
+<AppShell>
+	<!-- (header) -->
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { FileButton } from '@skeletonlabs/skeleton';
+	/**
+	 * @type {any}
+	 */
+	let files = [];
+</script>
+<div class="h-full w-full mx-auto flex justify-center items-center bg-black">
+
+		<div class="picker">
+			<FileButton name="files" 
+			multiple={true}
+			accept="image/*"
+			bind:files
+			on:change={() => {
+				console.log(files);
+			}}
+			/>
+		
+	</div>
+</div>
